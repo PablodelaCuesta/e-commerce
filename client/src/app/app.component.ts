51cb44222f6d9eeb40c1bae16c73897550a18e31
Pablo de la Cuesta <pablodelacuesta@hotmail.es>
2020-10-12 11:54:05 +0000

    angular basics

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/products';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'E-Commerce application';
+  // products: Array<any>;
+  products: IProduct[];
+
+  constructor(private http: HttpClient) {}
+  
+  ngOnInit(): void {
+    this.http.get("https://localhost:5001/api/products").subscribe( (response: any) => {
+      console.log(response);
+      this.products = response;
+    }, (error) => {
+      console.log(error);
+    });
+  }
+  
 }
